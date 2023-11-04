3454252ecb31f2cff5e7d5762fc2e5ffbd1706dd
iaan0607 <149846606+iaan0607@users.noreply.github.com>
2023-11-04 10:38:14 +0900

    1.py

--- 1.py
+++ 1.py
@@ -3,3 +3,14 @@ import pandas as pd
 import numpy as np
 
 st.title('ㄷㅇㅌㅍㄹㅇ ㅌㅌㄹㅇ')
+st.write("Hello **world**
+
+dataframe = pd.DataFrame({
+'first column':[1,2,3,4],
+'second column':[10,20,30,40],
+})
+
+st.dataframe(dataframe, use_container_width=False)
+
+st.table(dataframe)
+
