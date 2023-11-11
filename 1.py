5bea7e98f3dab8d4392a7f9beed217726cf31bb8
iaan0607 <149846606+iaan0607@users.noreply.github.com>
2023-11-11 09:31:55 +0900

    1.py

--- 1.py
+++ 1.py
@@ -15,3 +15,9 @@ st.dataframe(dataframe, use_container_width=True)
 st.table(dataframe)
 
 st.metric(label="온도",value="10°C",delta="-1.2°C")
+st.metric(label="삼성전자",value="61,000원",delta="-1,200원")
+
+col1,col2,col3 = st.columns(3)
+coll.metric(label="달러USD",value="1,228 원",delta="-12.00 원")
+col2.metric(label="일본JPY(100엔)",value="958.63 원",delta="-7.44 원")
+col3.metric(label="유럽연합EUR",value="1,335.82 원",delta="-11.44 원")
