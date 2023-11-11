788dd9bcd9ef5481f37137dfafe275bb4fa591a6
iaan0607 <149846606+iaan0607@users.noreply.github.com>
2023-11-11 09:46:41 +0900

    Update 1.py

--- 1.py
+++ 1.py
@@ -30,6 +30,6 @@ with tab1:
   st.header("A cat")
   st.image("https://static.streamlit.io/examples/cat.jpg",width=200)
 
-with tab2
+with tab2:
   st.header("A dog")
   st.image("https://static.streamlit.io/examples/dog.jpg",width=200)
